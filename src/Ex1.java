a192af7d5fa8b29684e85f4e9258fdffb82bd11a
Gashaw-ambaw <gashawambaw77@gmail.com>
2024-12-04 23:16:07 +0200

    Third commit

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,5 +1,5 @@
 public class Ex1{
     public static void main(String[] args){
-        System.out.println("Hi, my name is Gashao");
+        System.out.println("Hi, my name is Gashao, I like to place my phone far from my table when I study");
     }
 }
